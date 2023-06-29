1ed65a3b8fdbacb93cd090056466131b2799adc3
Dash <dkshowarth@gmail.com>
2023-06-29 12:05:24 -0700

    add initial variables, game function, while loop

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Rock-Paper-Scissors-
-This is a fun game to play Rock paper scissors 
+This is a fun game to play Rock paper scissors
+
+potential results:
+
+player picks scissors
+    comp rock = Loss
+    comp paper = win
+    <!-- comp scissors = tie -->
+player picks rock
+    <!-- comp rock = tie -->
+    comp paper = loss
+    comp scissors = win
+player picks paper
+    comp rock = win
+    <!-- comp paper = tie -->
+    comp scissors = loss
--- Script.js
+++ Script.js
@@ -1,1 +1,30 @@
-// 
+// We want to give a prompt to the user that lets them enter Rock, Paper, or Scissors
+// We randomly generate an opponent's move
+// We tell the player our move, and whether they won, tied or lost
+// We display their score for this session
+// We offer another round
+
+function playGame{
+    var playerWins;
+    var playerLosses;
+    var playerTies;
+    var playerMove;
+    var computerMove;
+
+    var possibleMoves = ["Rock", "Paper", "Scissors"];
+
+    function getRandomInt(){
+        //generate either 0, 1, or 2
+        return Math.floor(Math.random() * (3));
+    }
+
+    while(1){
+        playerMove = prompt("Please enter your choice below (Must be Rock, Paper, or Scissors)");
+        computerMove = possibleMoves[getRandomInt]
+        // if playermove === computermove, TIE
+        if (){
+
+        }
+        //if player hits 'cancel', break loop
+    }
+}
